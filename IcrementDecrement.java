bd54f1889b2fe82447d9be597d2ad0d82a7c0311
Khamkar Suraj Namdev <135463570+surajkhamkar06@users.noreply.github.com>
2024-03-24 00:00:51 +0530

    Add files via upload

---
+++ IcrementDecrement.java
@@ -0,0 +1,20 @@
+class IcrementDecrement
+{
+	public static void main(String[] args) {
+		
+		//int a = 5;
+		//int b = 5;
+		//int c = 5;
+		//int exp = a + b++ + ++c;
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		//
+		//int ++a = 100;
+		//System.out.println(++a);
+
+		
+
+
+	}
+}
