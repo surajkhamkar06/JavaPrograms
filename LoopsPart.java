ae42893f86ad2dec72b2491d8ce6b0d6f9c9d256
Khamkar Suraj Namdev <135463570+surajkhamkar06@users.noreply.github.com>
2024-04-01 21:42:49 +0530

    Add files via upload

---
+++ LoopsPart.java
@@ -0,0 +1,22 @@
+class LoopsPart
+{
+	public static void main(String[] args) {
+		
+		for(char ch = 'A'; ch<='Z'; ch++)
+		{
+			System.out.print(ch + " ");
+		}
+		System.out.println("************************");
+
+		for(char ch = 97; ch<=122; ch++)
+		{
+			System.out.print(ch + " ");
+		}
+		System.out.println("*************************");
+	
+	    for(char ch =40;ch<='9'; ch++)
+	    {
+	    	System.out.print(ch + " ");
+	    }
+}
+}
